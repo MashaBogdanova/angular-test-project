fdba39cbbf70dffc316db1ab56988186fb9e5823
Maria Bogdanova <masha.bogdanoff@gmail.com>
2024-02-10 18:51:21 +0100

    feat: add preloader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,35 @@
 import {Component, OnInit} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {NgForOf, TitleCasePipe} from "@angular/common";
+import {RouterOutlet} from '@angular/router';
+import {AsyncPipe, NgForOf, NgIf, TitleCasePipe} from "@angular/common";
 import {ProductComponent} from "./components/product/product.component";
 import {Product} from "./models/product";
 import {ProductsService} from "./services/products.service";
+import {Observable, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleCasePipe, ProductComponent, NgForOf],
+  imports: [RouterOutlet, TitleCasePipe, ProductComponent, NgForOf, NgIf, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   title = 'angular-test-project';
-  products: Product[] = [];
+  // products: Product[] = [];
+  products$: Observable<Product[]>;
+  loading = false;
 
   constructor(private productsService: ProductsService) {
   }
 
   ngOnInit(): void {
-    this.productsService.getAll().subscribe((products) => {
-      this.products = products;
-    });
+    this.loading = true;
+    this.products$ = this.productsService.getAll().pipe(
+      tap(() => this.loading = false)
+    );
+    // this.productsService.getAll().subscribe((products) => {
+    //   this.products = products;
+    //   this.loading = false;
+    // });
   }
 }
